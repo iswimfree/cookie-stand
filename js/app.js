982d1feb011c6246479c9bd7477cef6a2df0b7f0
michael <mikey.m.russell@gmail.com>
2020-12-07 21:20:52 -0800

    i made like 900 changes please note i deleted all code in app because i faile do trealize my condole log wasnt working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 'use strict';
 
+console.log('helllo world');
 
 
+
+// //create global variable th for hours
+// var hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1Pm', '2Pm', '3Pm', '4PM', '5PM', '6Pm', '7PM'];
+
+// //create min/max variable for each listed place Seattle ect..
+
+
+var seattleStore = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  salesPerhour: [],
+  dayTotal: 0,
+//   //now figure out how to get random number based on these numbers and previously established variable.
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+},
