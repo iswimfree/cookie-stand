5f606d6cbde6e67ea30663b118a18af7a7c53a20
michael <mikey.m.russell@gmail.com>
2020-12-07 21:37:40 -0800

    trying to get this online

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ console.log('helllo world');
 
 
 // //create global variable th for hours
-// var hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1Pm', '2Pm', '3Pm', '4PM', '5PM', '6Pm', '7PM'];
+var hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1Pm', '2Pm', '3Pm', '4PM', '5PM', '6Pm', '7PM'];
 
 // //create min/max variable for each listed place Seattle ect..
 
@@ -17,9 +17,11 @@ var seattleStore = {
   avg: 6.3,
   salesPerhour: [],
   dayTotal: 0,
-//   //now figure out how to get random number based on these numbers and previously established variable.
+  //   //now figure out how to get random number based on these numbers and previously established variable.
+  // function getRandomInt(max, min) {
+  //   return Math.floor(Math.random(min) * Math.floor(max))
+  //   console.log(getRandomInt());
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-},
+  // i have no clue how to do this and im done trying at this point will continue later
+
+// pretty sure my console.log isnt even working so i cant even tell if im doing it right
